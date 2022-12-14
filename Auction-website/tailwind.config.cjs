24347692cd3b3a62c67ac4d02db9c7315eed7a38
Christinalulu <christinalu.fugel96@gmail.com>
2022-12-14 17:02:24 +0100

    Made a Vite project, Installed tailwindcss, and got my colors and fonts in place

---
+++ Auction-website/tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.{html,js}", "./js/*.js"],
+  theme: {
+    extend: {
+      colors:{
+        "primary": "#CED7D9",
+        "main-action": "#2F67BB",
+        "second-action": "#004C81",
+        
+      }
+      
+    },
+  },
+  plugins: [],
+};
+
