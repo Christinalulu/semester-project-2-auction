685d6e19687ac95e7f48630ce14afeec91dc8cec
Christinalulu <christinalu.fugel96@gmail.com>
2022-12-16 13:57:57 +0100

    front page

--- Auction-website/js/main.js
+++ Auction-website/js/main.js
@@ -1,1 +1,10 @@
 import '../style.css'
+
+const toggelMenu = document.getElementById("toggel-btn-menu");
+const navBarMenu = document.getElementById("mobile-menu");
+console.log(toggelMenu);
+console.log(navBarMenu);
+toggelMenu.addEventListener('click', function(){
+   navBarMenu.classList.toggle('hidden')
+})
+
